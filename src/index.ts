4b90e1dc76390f4762e7e76f90d55a780c91fcf3
giancode1 <gieg5c@gmail.com>
2022-04-21 23:53:32 -0500

    schema composition

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,27 @@ const arraySchema = z.array(numberSchema)
 const undefinedSchema = z.undefined()
 const nullSchema = z.null()
 
-const UserSchema = z.object({
-    name: nameSchema,
-    email: z.string().email(),
-    phone: z.number().positive(),
-})
 //const name = 100
 //nameSchema.parse(name) //ZodError
 
 const result = nameSchema.parse('Gian') 
 console.log(result)
 
+const UserSchema = z.object({
+    name: nameSchema,
+    email: z.string().email(),
+    phone: z.number().positive(),
+})
+
+const addressSchema = z.object({
+    street: z.string(),
+    city: z.string(),
+})
+
+const UserCitySchema = UserSchema.merge(addressSchema) //merge: combina los esquemas
 
 type UserType = z.infer<typeof UserSchema>
+type UserCityType = z.infer<typeof UserCitySchema>
 
 const User1: UserType = {
     name: 'Gian',
@@ -29,5 +37,14 @@ const User1: UserType = {
     //age:20
 }
 
-const reulst2 = UserSchema.parse(User1)
+const result1 = UserSchema.parse(User1)
+console.log('UserSchema:', result1)
 
+
+const UserCity: UserCityType = {
+    ...User1,
+    street: 'Calle 123',
+    //city: 'Ciudad',  //path:[city]  , message: required
+}
+const result2 = UserCitySchema.parse(UserCity)
+console.log('UserCitySchema:', result2)
