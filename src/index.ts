207a14de36d38c3b99d53875dde28c0263bf3198
giancode1 <gieg5c@gmail.com>
2022-04-21 23:31:13 -0500

    Type Inference

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import {z} from 'zod'
 const nameSchema = z.string()
 const numberSchema = z.number()
 const booleanSchema = z.boolean()
-const objectSchema = z.object({
-    name: nameSchema,
-})
+
 const arraySchema = z.array(numberSchema)
 const undefinedSchema = z.undefined()
 const nullSchema = z.null()
 
+const UserSchema = z.object({
+    name: nameSchema,
+    email: z.string().email(),
+    phone: z.number().positive(),
+})
 //const name = 100
 //nameSchema.parse(name) //ZodError
 
@@ -17,5 +20,14 @@ const result = nameSchema.parse('Gian')
 console.log(result)
 
 
+type UserType = z.infer<typeof UserSchema>
 
+const User1: UserType = {
+    name: 'Gian',
+    email: 'gian@gmail.com',
+    phone: 123456789,
+    //age:20
+}
+
+const reulst2 = UserSchema.parse(User1)
 
