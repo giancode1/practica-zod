9590170fca9203a8201cb5505041205a2d37090e
giancode1 <gieg5c@gmail.com>
2022-04-21 23:08:37 -0500

    ejemplos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello')
+import {z} from 'zod'
+
+const nameSchema = z.string()
+const numberSchema = z.number()
+const booleanSchema = z.boolean()
+const objectSchema = z.object({
+    name: nameSchema,
+})
+const arraySchema = z.array(numberSchema)
+const undefinedSchema = z.undefined()
+const nullSchema = z.null()
+
+//const name = 100
+//nameSchema.parse(name) //ZodError
+
+const result = nameSchema.parse('Gian') 
+console.log(result)
+
+
+
+
